888dbf254fb8f3f8aec09f1ebf4cde62c3849f36
Dipesh Patel <dipesh@rapidops.com>
2021-06-17 22:22:11 +0530

    updated production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiHost: 'https://demo.vendure.io',
+    apiHost: 'https://store1.rapidretail.io',
     apiPort: 443,
     shopApiPath: 'shop-api',
     baseHref: '/storefront/',

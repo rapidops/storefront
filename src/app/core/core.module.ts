d7ade6b7d73cd81561ab04ef3f0bd73afd05a72e
ekta Kanojiya <ekta.kanojiya@rapidops.com>
2021-06-18 17:24:45 +0530

    #NoTicket display banners from the Admin

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -30,6 +30,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 import { ProductListControlsComponent } from './components/product-list-controls/product-list-controls.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductSearchBarComponent } from './components/product-search-bar/product-search-bar.component';
+import {BannerComponent} from './components/banner/banner.component';
 import { buildIconLibrary } from './icon-library';
 import { DefaultInterceptor } from './providers/data/interceptor';
 
@@ -51,6 +52,7 @@ const CORE_COMPONENTS = [
     ProductListControlsComponent,
     ProductSearchBarComponent,
     AssetGalleryComponent,
+    BannerComponent
 ];
 
 let apolloCache: InMemoryCache;

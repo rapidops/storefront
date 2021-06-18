7cfafc330ac071bffb14a01095d8f3387f4112f8
ekta Kanojiya <ekta.kanojiya@rapidops.com>
2021-06-18 19:17:48 +0530

    #NoTicket added script for menu

--- src/app/core/components/collections-menu/collections-menu.component.ts
+++ src/app/core/components/collections-menu/collections-menu.component.ts
@@ -1,16 +1,18 @@
 import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { DOCUMENT } from '@angular/common';
+import {isPlatformBrowser} from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
     Inject,
     OnDestroy,
-    OnInit,
+    OnInit, PLATFORM_ID,
     TemplateRef,
     ViewChild,
     ViewContainerRef,
 } from '@angular/core';
+import {DeviceDetectorService} from 'ngx-device-detector';
 import { Observable, Subject } from 'rxjs';
 import { debounceTime, map, takeUntil } from 'rxjs/operators';
 
@@ -19,8 +21,8 @@ import { GET_COLLECTIONS } from '../../../common/graphql/documents.graphql';
 import { DataService } from '../../../core/providers/data/data.service';
 
 import { arrayToTree, RootNode, TreeNode } from './array-to-tree';
-// import {isPlatformBrowser} from '@angular/common';
-// declare let $;
+declare let $: any;
+
 @Component({
     selector: 'vsf-collections-menu',
     templateUrl: './collections-menu.component.html',
@@ -38,11 +40,16 @@ export class CollectionsMenuComponent implements OnInit, OnDestroy {
     private overlayIsOpen$ = new Subject<boolean>();
     private setActiveCollection$ = new Subject<TreeNode<GetCollections.Items>>();
     private destroy$ = new Subject();
+    isMobile = this.deviceDetectorService.isMobile();
+    isIpad = this.deviceDetectorService.isTablet();
+    isDesktop = this.deviceDetectorService.isDesktop();
 
     constructor(@Inject(DOCUMENT) private document: Document,
+                @Inject(PLATFORM_ID) private _platformId: Object,
                 private dataService: DataService,
                 private overlay: Overlay,
-                private viewContainerRef: ViewContainerRef) { }
+                private viewContainerRef: ViewContainerRef,
+                private deviceDetectorService: DeviceDetectorService) { }
 
     ngOnInit() {
         // this.setMenuMouseEvents();
@@ -69,6 +76,7 @@ export class CollectionsMenuComponent implements OnInit, OnDestroy {
         ).subscribe(val => {
             this.activeCollection = val;
         });
+        this.setMenuMouseEvents();
     }
 
     ngOnDestroy(): void {
@@ -99,87 +107,97 @@ export class CollectionsMenuComponent implements OnInit, OnDestroy {
     close(event: any) {
         this.overlayIsOpen$.next(false);
     }
-    // setMenuMouseEvents() {
-    //     const isMobile = this.isMobile;
-    //     const isIpad = this.isIpad;
-    //     const isDesktop = this.isDesktop;
-    //     if (isPlatformBrowser(this._platformId)) {
-    //         $(document).ready(function () {
-    //             $(document).on('click', '.nav-open', function () {
-    //                 const window_height = $(window).height();
-    //                 $(this).next().toggleClass('active');
-    //                 $('body').toggleClass('bm-overlay-body');
-    //                 const headerHeight = $('.main-header').height();
-    //                 $('.hts-perfect-scrollbar').css({'height': window_height - (headerHeight + 80)}); // 80 is the height of Browse by category title when menu opens
-    //
-    //                 if (isDesktop) {
-    //                     const menu_height = $('.hts-perfect-scrollbar').height();
-    //                     $('.perfect-scrollbar-submenu').css({'max-height': menu_height - 63});
-    //                 }
-    //             });
-    //             // For the mobile view
-    //             // TODO  need to do dynamic mobile view
-    //             if (isMobile && window.innerWidth > 1023) {
-    //                 $(document).on('click', '.close', function () {
-    //                     if ($('.side-megamenu').hasClass('active')) {
-    //                         $('.side-megamenu').removeClass('active');
-    //                         $('body').toggleClass('bm-overlay-body');
-    //                     }
-    //                 });
-    //             } else if (isMobile) {
-    //                 $(document).on('click', '.for-mobile .close', function () {
-    //                     if ($('.side-megamenu').hasClass('active')) {
-    //                         $('.side-megamenu').removeClass('active');
-    //                         $('body').toggleClass('bm-overlay-body');
-    //                     }
-    //                 });
-    //             } else {
-    //                 $(document).on('click', '.close', function () {
-    //                     if ($('.side-megamenu').hasClass('active')) {
-    //                         $('.side-megamenu').removeClass('active');
-    //                         $('body').toggleClass('bm-overlay-body');
-    //                     }
-    //                 });
-    //             }
-    //             // For the mobile view
-    //             $(document).on('click', '.menutitle', function () {
-    //                 $(this).next().toggleClass('active');
-    //                 $('.menutitle').toggleClass('active');
-    //             });
-    //             $(document).on('click', 'ul.menu-nav li .category-details li a', function () {
-    //                 // close menu while selecting category
-    //                 if ($('.side-megamenu').hasClass('active')) {
-    //                     $('.side-megamenu').removeClass('active');
-    //                     $('body').toggleClass('bm-overlay-body');
-    //                 }
-    //             });
-    //
-    //             if (isDesktop) {
-    //                 // for display some menu besides of main menu
-    //                 $(document).on('mouseover', 'ul.menu-nav li a', function () {
-    //
-    //                     const window_height = $('.side-megamenu').height();
-    //                     const top_position = $(this).position().top;
-    //                     $('.category-details').css({'max-height': window_height});
-    //                     if (top_position > (window_height / 2)) { // set bottom if selected category's height is  > window's height
-    //                         $(this).next('.category-details').css({'bottom': 0, 'top': 'inherit'});
-    //
-    //                     } else {
-    //                         $(this).next('.category-details').css({'top': 0, 'bottom': 'inherit'});
-    //                     }
-    //                 });
-    //             }
-    //
-    //             if (isMobile || isIpad) { // no need to display subcategories HTDART-15921
-    //                 $(document).on('click', 'ul.menu-nav li', function () {
-    //                     // $(this).toggleClass('active'); // Hide submenu for mobile
-    //                 });
-    //                 const window_height = $(window).height();
-    //                 $('.side-megamenu').css({'max-height': window_height - 150});
-    //             }
-    //         });
-    //     }
-    // }
+    setMenuMouseEvents() {
+         const isMobile = this.isMobile;
+        const isIpad = this.isIpad;
+        const isDesktop = this.isDesktop;
+        if (isPlatformBrowser(this._platformId)) {
+            // tslint:disable-next-line:only-arrow-functions
+            $(document).ready(() => {
+                $(document).on('click', '.nav-open', function () {
+                    const window_height = $(window).height();
+                    // @ts-ignore
+                    $(this).next().toggleClass('active');
+                    $('body').toggleClass('bm-overlay-body');
+                    const headerHeight = $('.main-header').height();
+                    $('.hts-perfect-scrollbar').css({'height': window_height - (headerHeight + 80)}); // 80 is the height of Browse by category title when menu opens
+
+                    if (isDesktop) {
+                        const menu_height = $('.hts-perfect-scrollbar').height();
+                        $('.perfect-scrollbar-submenu').css({'max-height': menu_height - 63});
+                    }
+                });
+                // For the mobile view
+                // TODO  need to do dynamic mobile view
+                if (isMobile && window.innerWidth > 1023) {
+                    // tslint:disable-next-line:only-arrow-functions
+                    $(document).on('click', '.close', function () {
+                        if ($('.side-megamenu').hasClass('active')) {
+                            $('.side-megamenu').removeClass('active');
+                            $('body').toggleClass('bm-overlay-body');
+                        }
+                    });
+                } else if (isMobile) {
+                    // tslint:disable-next-line:only-arrow-functions
+                    $(document).on('click', '.for-mobile .close', function () {
+                        if ($('.side-megamenu').hasClass('active')) {
+                            $('.side-megamenu').removeClass('active');
+                            $('body').toggleClass('bm-overlay-body');
+                        }
+                    });
+                } else {
+                    // tslint:disable-next-line:only-arrow-functions
+                    $(document).on('click', '.close', function () {
+                        if ($('.side-megamenu').hasClass('active')) {
+                            $('.side-megamenu').removeClass('active');
+                            $('body').toggleClass('bm-overlay-body');
+                        }
+                    });
+                }
+                // For the mobile view
+                $(document).on('click', '.menutitle', function () {
+                    // @ts-ignore
+                    $(this).next().toggleClass('active');
+                    $('.menutitle').toggleClass('active');
+                });
+                // tslint:disable-next-line:only-arrow-functions
+                $(document).on('click', 'ul.menu-nav li .category-details li a', function () {
+                    // close menu while selecting category
+                    if ($('.side-megamenu').hasClass('active')) {
+                        $('.side-megamenu').removeClass('active');
+                        $('body').toggleClass('bm-overlay-body');
+                    }
+                });
+
+                if (isDesktop) {
+                    // for display some menu besides of main menu
+                    $(document).on('mouseover', 'ul.menu-nav li a', () => {
+
+                        const window_height = $('.side-megamenu').height();
+                        // @ts-ignore
+                        const top_position = $(this).position().top;
+                        $('.category-details').css({'max-height': window_height});
+                        if (top_position > (window_height / 2)) { // set bottom if selected category's height is  > window's height
+                            // @ts-ignore
+                            $(this).next('.category-details').css({'bottom': 0, 'top': 'inherit'});
+
+                        } else {
+                            $(this).next('.category-details').css({'top': 0, 'bottom': 'inherit'});
+                        }
+                    });
+                }
+
+                if (isMobile || isIpad) { // no need to display subcategories HTDART-15921
+                    // tslint:disable-next-line:only-arrow-functions
+                    $(document).on('click', 'ul.menu-nav li', function () {
+                        // $(this).toggleClass('active'); // Hide submenu for mobile
+                    });
+                    const window_height = $(window).height();
+                    $('.side-megamenu').css({'max-height': window_height - 150});
+                }
+            });
+        }
+    }
 
     private openOverlay() {
         if (this.closeFn) {
